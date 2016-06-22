ca44dd04ee3e909cba11b4361194bce717953cbb
ravenusmc <mcuddy77@gmail.com>
2016-06-22 15:15:50 -0400

    bootstrap and semantic UI gems added

--- Gemfile
+++ Gemfile
@@ -45,3 +45,6 @@ group :development do
   gem 'spring'
 end
 
+gem 'bootstrap-sass'
+gem 'semantic-ui-sass'
+
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
+//= require semantic-ui
 //= require turbolinks
 //= require_tree .
