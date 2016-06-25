17b407f053559c329a3e74950ad7aa4be0a1c35f
ravenusmc <mcuddy77@gmail.com>
2016-06-25 14:12:32 -0400

    added a new and create actions and their respective pages

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,4 +12,19 @@ class GroupsController < ApplicationController
     @group = Group.new 
   end 
 
+  def create
+    @group = Group.new(group_params)
+    if @group.save 
+      redirect_to @group 
+    else 
+      render 'new'
+    end
+  end 
+
+  private
+
+    def group_params
+      params.require(:group).permit(:topic, :user_id, :description) 
+    end
+
 end
