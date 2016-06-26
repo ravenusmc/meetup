c7bcc3ed250d8b58d6b1064ff80fd2219a54e453
ravenusmc <mcuddy77@gmail.com>
2016-06-26 14:00:20 -0400

    worked on comments new action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,17 @@
 class CommentsController < ApplicationController
 
   def new 
+    @comment = Comment.new(group: @group)
   end 
 
   def create
+    @groups = Group.all
+    @comment = Comment.new(comment_params)
+    if @comment.save 
+      redirect_to @group 
+    else 
+      render 'new'
+    end 
   end 
 
   def edit 
@@ -11,5 +19,11 @@ class CommentsController < ApplicationController
 
   def update
   end 
+
+  private 
+
+    def comment_params 
+      params.require(:comment).permit(:author, :comment, :user_id, :group_id)
+    end 
 
 end
