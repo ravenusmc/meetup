82249f475dfa22c37ecdd6719eeea645e588b9ca
ravenusmc <mcuddy77@gmail.com>
2016-06-26 17:46:34 -0400

    added validations to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :group 
+
+  validates :author, presence: true 
+
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -5,6 +5,9 @@ class Group < ActiveRecord::Base
 
   validates :topic, presence: true
   validates :description, presence: true, length: { minimum: 10 }
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
 
   geocoded_by :address
   after_validation :geocode
